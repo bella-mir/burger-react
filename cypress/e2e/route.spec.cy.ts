22542c9c9bfa3bad2f544bea30f2ae7c8b4fc368
Bella Mironova <bellamironova@gmail.com>
2023-04-02 22:06:59 +0300

    tests improved

--- cypress/e2e/route.spec.cy.ts
+++ cypress/e2e/route.spec.cy.ts
@@ -10,17 +10,17 @@ describe("availability opf service", function () {
     cy.contains("Соберите бургер");
   });
 
-  it('"Лента заказов" opens  when we click on "Лента заказов" in a header', function () {
-    cy.viewport(1655, 1080);
-    cy.visit("http://localhost:3000/burger-react");
-    cy.get("header").contains("Лента заказов").click();
-    cy.contains("Лента заказов");
-  });
-
   it('Login page opens when we click on "Войти" in a header ', function () {
     cy.viewport(1655, 1080);
     cy.visit("http://localhost:3000/burger-react");
     cy.get("header").contains("Войти").click();
     cy.contains("Вход");
+  });
+
+  it('"Лента заказов" opens  when we click on "Лента заказов" in a header', function () {
+    cy.viewport(1655, 1080);
+    cy.visit("http://localhost:3000/burger-react");
+    cy.get("header").contains("Лента заказов").click();
+    cy.contains("Лента заказов");
   });
 });
